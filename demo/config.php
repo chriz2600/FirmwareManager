c03e59302081414902b4fc74bd171fb587cc4702
Christof Harnischmacher <ch@cm4all.com>
2017-11-17 00:15:00 +0100

    setup mode with current values

---
+++ demo/config.php
@@ -0,0 +1,14 @@
+<?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    header('Content-Type: text/json');
+
+    if (!$_SESSION["config"]) {
+        $_SESSION["config"] = '{"ssid":"SomeSSID","password":"SomePassword","ota_pass":"SomeOTAPassword","firmware_url":"http://dc.i74.de/firmware.rbf","http_auth_user":"Test","http_auth_pass":"testtest"}';
+    } else {
+        error_log("c---> " . $_SESSION["config"]);
+    }
+
+    echo $_SESSION["config"];
--- demo/setup.php
+++ demo/setup.php
@@ -1,2 +1,29 @@
 <?php
+    function get($name, $defaultValue) {
+        global $_POST, $j;
+        if (isset($_POST[$name])) {
+            if ($_POST[$name] == "") {
+                $j[$name] = $defaultValue;
+            } else {
+                $j[$name] = $_POST[$name];
+            }
+        }
+    }
+
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    if ($_SESSION["config"]) {
+        $j = json_decode($_SESSION["config"], true);
+    }
+
+    get("ssid", "");
+    get("password", "");
+    get("ota_pass", "");
+    get("firmware_url", "http://dc.i74.de/firmware.rbf");
+    get("http_auth_user", "Test");
+    get("http_auth_pass", "testtest");
+    
+    $_SESSION["config"] = json_encode($j);
     echo "OK";
